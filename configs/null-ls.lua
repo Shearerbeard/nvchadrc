224dac7dcdccb06c831f7c9c8f11ad24219c7393
Michael Shearer <mshearer@pesa.com>
2023-11-28 15:28:16 -0700

    more fmt

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,30 +1,30 @@
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 local actions = null_ls.builtins.code_actions
 
 local sources = {
-	formatting.prettier,
-	formatting.eslint_d,
-	formatting.rustywind,
-	formatting.stylua,
-	formatting.rustfmt,
-	formatting.black,
-	formatting.stylish_haskell,
-	formatting.cmake_format,
+  formatting.prettier,
+  formatting.eslint_d,
+  formatting.rustywind,
+  formatting.stylua,
+  formatting.rustfmt,
+  formatting.black,
+  formatting.stylish_haskell,
+  formatting.cmake_format,
 
-	lint.pylint,
-	lint.cpplint,
-	lint.shellcheck,
-	lint.eslint_d,
-	lint.luacheck,
-	lint.cmake_lint,
+  lint.pylint,
+  lint.cpplint,
+  lint.shellcheck,
+  lint.eslint_d,
+  lint.luacheck,
+  lint.cmake_lint,
 
-	actions.eslint_d,
+  actions.eslint_d,
 }
 
-null_ls.setup({
-	debug = true,
-	sources = sources,
-})
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
